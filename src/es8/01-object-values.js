5ee642f4281783d3aab997dbc84d9c59abc206ee
walternomas <w.barriosvazquez@gmail.com>
2022-08-19 10:12:38 -0300

    es8 object values

---
+++ src/es8/01-object-values.js
@@ -0,0 +1,10 @@
+const countries = {
+  MX: "Mexico",
+  CO: "Colombia",
+  CL: "Chile",
+  PE: "Peru",
+  UY: "Uruguay",
+  CA: "Canada",
+  JS: "Japan",
+};
+console.log(Object.values(countries));
