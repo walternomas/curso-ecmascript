0ea77d37e9053bd348f79f1c6ea5b03d16fc5f24
walternomas <w.barriosvazquez@gmail.com>
2022-08-19 07:11:54 -0300

    template literals

---
+++ src/es6/03-strings.js
@@ -0,0 +1,17 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase);
+
+// Template literals
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// Multiline strings
+let lorem = 'esto es un string \n ' + 'esto es otra linea';
+console.log(lorem);
+
+let lorem2 = `esto es una frase epica
+ la continuacion de esa frase epica.
+`;
+console.log(lorem2);
