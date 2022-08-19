045aec6370232dbe6478dedec54d9ae032c37efa
walternomas <w.barriosvazquez@gmail.com>
2022-08-19 07:22:50 -0300

    rest spread

---
+++ src/es6/05-rest-spread.js
@@ -0,0 +1,11 @@
+// Array destructuring
+
+let fruits = ['Apple', 'Banana'];
+let [a,b] = fruits;
+console.log(a, fruits[1]);
+
+// Object destructuring
+
+let user = { username: 'Walter', age : 44};
+let {username,age} = user;
+console.log(username, user.age);
