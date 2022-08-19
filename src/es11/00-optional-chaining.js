be7161228f8251df2412e1fb4d54ce26d6547e30
walternomas <w.barriosvazquez@gmail.com>
2022-08-19 13:00:26 -0300

    es11 optional chaining

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,12 @@
+// junio 2020
+const users = {
+  walternomas: {
+    country: 'AR'
+  },
+  mariana: {
+    country: 'CO'
+  }
+}
+
+console.log(users.walternomas.age);
+console.log(users?.juli?.country);
