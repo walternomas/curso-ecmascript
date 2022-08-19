f88784eca65f9956b666266682f5495b5dfa25f7
walternomas <w.barriosvazquez@gmail.com>
2022-08-19 10:06:16 -0300

    es7 array includes

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,6 @@
+const numbers = [1,3,4,6,7,8];
+console.log(numbers.includes(4));
+console.log(numbers.includes(9));
+
+const list = ['walte', 'juli', 'jaz', 'marian'];
+console.log(list.includes('Walte'));
