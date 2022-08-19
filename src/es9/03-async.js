64ccf66c5e6e8617a9ce041900705496e6ba4616
walternomas <w.barriosvazquez@gmail.com>
2022-08-19 11:23:32 -0300

    es9 async array

--- src/es9/03-async.js
+++ src/es9/03-async.js
@@ -9,3 +9,12 @@ other.next().then((response) => console.log(response.value));
 other.next().then((response) => console.log(response.value));
 other.next().then((response) => console.log(response.value));
 console.log("Hello!");
+
+async function arrayOfNames(array) {
+  for await (let value of array) {
+    console.log(value);
+  }
+}
+
+const names = arrayOfNames(["Walter", "Julian", "Jazmine", "Mariana"]);
+console.log("After");
